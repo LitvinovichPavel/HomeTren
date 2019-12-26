0659311b958d4a96bf0712839fe089a04ebe7075
Pavel Litvinovich <21asdf@mail.ru>
2019-12-26 20:51:21 +0300

    StringBuilder

---
+++ lecture_08/lecture_08/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+
+namespace lecture_08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str2 = "AAA.";
+            StringBuilder sb = new StringBuilder();
+            sb.Append(str2).Append(str2).Append(str2).Append(str2).Append(str2);
+            Console.WriteLine(sb);
+            sb.Clear();
+            Console.WriteLine("Clear" + sb);
+        }
+    }
+}
