49a5e3e8198ed3d07358a6c0734c5ab5486aee60
Pavel Litvinovich <21asdf@mail.ru>
2019-12-30 16:28:52 +0300

    hm_8

---
+++ hm_8/hm_8/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace hm_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string strSymbol = "1a!2.3!!.. 4.!.?6 7! ..?";
+            Task1(strSymbol);
+        }
+
+        static void Task1(string strSymbol)
+        {
+            string str1 = strSymbol;
+            StringBuilder sb = new StringBuilder();
+
+
+
+            Console.WriteLine(str1);
+        }
+    }
+}
---
+++ methodTren/methodTren/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace methodTren
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+
+    }
+}
