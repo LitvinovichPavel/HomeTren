8b89f733e8de51943d6c76676f9d95b29753b966
Pavel Litvinovich <21asdf@mail.ru>
2019-12-30 15:18:34 +0300

    +null

--- stringTren/stringTren/Program.cs
+++ stringTren/stringTren/Program.cs
@@ -6,7 +6,17 @@ namespace stringTren
     {
         static void Main(string[] args)
         {
-            Console.Write(new string('*', 100000000));
+            Console.WriteLine(new string('*', 10));
+
+            char[] ch = "Hello".ToCharArray();
+            string str = new string(ch); //конструируем строку из массива
+            Console.WriteLine(str);
+
+            //строки null и пустые строки
+            string str1 = "";
+            Console.WriteLine(str1 == ""); //true
+            Console.WriteLine(str1 == string.Empty); //true
+            Console.WriteLine(str1.Length == 0); //true
         }
     }
 }
