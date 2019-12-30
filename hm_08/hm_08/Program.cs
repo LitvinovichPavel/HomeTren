d9c37a8e725a023f4fa9813320304ed8b072227c
Pavel Litvinovich <21asdf@mail.ru>
2019-12-30 17:35:53 +0300

    hm_8

---
+++ hm_08/hm_08/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+
+namespace hm_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string strSymbol = "1a!2.3!!.. 4.!.?6 7! ..?";
+            Task1(strSymbol);
+        }
+
+        static void Task1(string strSymbol)
+        {
+            string str1 = strSymbol;
+            Console.WriteLine(str1);
+            StringBuilder sb = new StringBuilder();
+            int indexTurn = str1.IndexOf("?"); //определяем положение вопросительного знака (?) в строке
+
+            for (int i = 0; i < indexTurn; i++)
+            {
+                if (str1[i] == '!')
+                {
+                    str1 = str1.Replace("!", string.Empty);
+                }
+            }
+
+            Console.WriteLine("str1:\n" + str1);
+            Console.WriteLine();
+
+
+        }
+    }
+}
