097abfa2b6a645010c26279db00a692ffec7450f
Pavel Litvinovich <21asdf@mail.ru>
2019-12-30 15:10:17 +0300

    повтор кол-во раз

---
+++ stringTren/stringTren/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace stringTren
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(new string('*', 100000000));
+        }
+    }
+}
