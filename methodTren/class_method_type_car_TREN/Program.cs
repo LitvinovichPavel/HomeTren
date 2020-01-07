b5ba7d5aa25a20abad77e258a88b727f0cf20a13
Pavel Litvinovich <21asdf@mail.ru>
2020-01-07 18:15:38 +0300

    class.method.cars.new

---
+++ methodTren/class_method_type_car_TREN/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace class_method_type_car_TREN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SUV toyotaSequoia = new SUV();
+            toyotaSequoia.ToyotaSequoia();
+        }
+    }
+}
---
+++ methodTren/class_method_type_car_TREN/SUV.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace class_method_type_car_TREN
+{
+    class SUV
+    {
+        public string mark;
+        public string model;
+        public double volume;
+        public int maxSpeed;
+        public int mass;
+
+        public SUV()
+        {
+            this.mark = mark;
+            this.model = model;
+            this.volume = volume;
+            this.maxSpeed = maxSpeed;
+            this.mass = mass;
+        }
+        public void ToyotaSequoia()
+        {
+            mark = "Toyota";
+            model = "Sequoia";
+            volume = 5.7;
+            maxSpeed = 180;
+            mass = 3300;
+            Console.WriteLine($"{mark} {model} {volume} liters, max speed {maxSpeed} km/h, mass {mass} kg");
+        }
+    }
+}
