09c70e11766b3ba1a28846180dd855cfffeebe7e
Pavel Litvinovich <21asdf@mail.ru>
2020-01-07 23:18:07 +0300

    Troelsen.static.1

---
+++ methodTren/Troelsen.static.1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Troelsen.@static._1
+{
+    class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
+}
---
+++ methodTren/Troelsen_static_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Troelsen_static_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
