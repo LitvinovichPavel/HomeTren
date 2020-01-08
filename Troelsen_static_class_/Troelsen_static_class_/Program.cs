9c8493cd1576e7cd638ee683716de4e9b2985279
Pavel Litvinovich <21asdf@mail.ru>
2020-01-08 15:19:33 +0300

    static.class.99

---
+++ Troelsen_static_class_/Troelsen_static_class_/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Troelsen_static_class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeUtilClass.PrintDate();
+            TimeUtilClass.PrintTime();
+
+            //TimeUtilClass u = new TimeUtilClass();   нельзя создать экземпляр статического класса
+        }
+    }
+    static class TimeUtilClass
+    {
+        public static void PrintTime()
+        {
+            Console.WriteLine(DateTime.Today.ToShortTimeString());
+        }
+        public static void PrintDate()
+        {
+            Console.WriteLine(DateTime.Today.ToShortDateString());
+        }
+    }
+}
+
