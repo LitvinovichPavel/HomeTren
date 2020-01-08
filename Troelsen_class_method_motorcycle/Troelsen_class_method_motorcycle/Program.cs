7f6ea412cbecde17ce0ad436ee8545b9138af6bd
Pavel Litvinovich <21asdf@mail.ru>
2020-01-08 15:27:40 +0300

    Troelsen.class.method.motorcycle

---
+++ Troelsen_class_method_motorcycle/Troelsen_class_method_motorcycle/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace AssemblyTwoTREN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Motorcycle mc = new Motorcycle(5);
+            mc.PopWheely();
+            mc.SetDriveName("POL");
+            Console.WriteLine($"Name: {mc.name}");
+        }
+    }
+    class Motorcycle
+    {
+        public int driveIntensity;
+        public string name;
+
+        public Motorcycle(int intensity)
+        {
+            driveIntensity = intensity;
+        }
+
+        public void SetDriveName(string name)
+        {
+            this.name = name;
+        }
+
+        public void PopWheely()
+        {
+            for (int i = 0; i < driveIntensity; i++)
+            {
+                Console.WriteLine("MOTORCUCLE");
+            }
+        }
+    }
+}
