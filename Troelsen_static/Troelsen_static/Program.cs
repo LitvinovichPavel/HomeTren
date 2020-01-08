d878a6bc93791f502f9db327e93f2c4a888da18e
Pavel Litvinovich <21asdf@mail.ru>
2020-01-08 15:06:01 +0300

    Troelsen.static.method.constryctor.99

---
+++ Troelsen_static/Troelsen_static/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Troelsen_static_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SavingAccount s1 = new SavingAccount(50);
+
+            Console.WriteLine($"Interest Rate is: {SavingAccount.GetInterestRate()}");
+            SavingAccount.SetInterestRate(0.08);
+
+            SavingAccount s2 = new SavingAccount(100);
+            Console.WriteLine($"Interest Rate is: {SavingAccount.GetInterestRate()}");
+        }
+        class SavingAccount
+        {
+            public double currBalance;
+            public static double currInterestRate;
+            public SavingAccount(double balance)   //конструктор
+            {
+                currBalance = balance;
+            }
+            static SavingAccount()         //конструктор
+            {
+                Console.WriteLine($"In static ctor!");
+                currInterestRate = 0.04;
+            }
+            public static void SetInterestRate(double newRate)
+            {
+                currInterestRate = newRate;
+            }
+            public static double GetInterestRate()
+            {
+                return currInterestRate;
+            }
+        }
+    }
+}
