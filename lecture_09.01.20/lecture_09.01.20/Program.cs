57a7d60488f37e7d28efcd0f4c014572413ffec9
Pavel Litvinovich <21asdf@mail.ru>
2020-01-09 21:45:47 +0300

    modificators.1

---
+++ lecture_09.01.20/lecture_09.01.20/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lecture_09._01._20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 class1 = new Class1();
+            class1.x = 22;
+        }
+    }
+    class Class1
+    {
+        public int x = 11;
+    }
+}
