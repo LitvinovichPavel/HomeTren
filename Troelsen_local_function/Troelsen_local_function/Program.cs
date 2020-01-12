7facccbec3ae5628eb3820c53a1d32aae9a22f3e
Pavel Litvinovich <21asdf@mail.ru>
2020-01-12 13:03:42 +0300

    Trorlsen.local.function

---
+++ Troelsen_local_function/Troelsen_local_function/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace lecture_09._01._20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(AddWrapper(3, 7));
+        }
+        static int AddWrapper(int x, int y)
+        {
+            return Add();
+            int Add()
+            {
+                return x + y;
+            }
+        }
+    }
+}
