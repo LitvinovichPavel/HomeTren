eb91d099aa57cf3ba7c795e89cd1b8d8d5337fef
Pavel Litvinovich <21asdf@mail.ru>
2020-01-16 17:14:20 +0300

    Troelsen.Tuples.1

---
+++ Troelsen_Tuples/Troelsen_Tuples/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Troelsen_Tuples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tuples_ tuples_ = new Tuples_();
+            tuples_.MethodTuples();
+        }
+    }
+    class Tuples_
+    {
+        public void MethodTuples()
+        {
+            (string, int, string) value = ("A", 21, "C");
+            var value2 = (ax: "ab", dax: 121, bex: "cd");
+            Console.WriteLine(value.Item1);
+            Console.WriteLine(value.Item2);
+            Console.WriteLine(value.Item3);
+
+            Console.WriteLine(value2.ax);
+            Console.WriteLine(value2.dax);
+            Console.WriteLine(value2.bex);
+        }
+    }
+}
