1b13e6c566f2f79c1b9a7ada1595540cb8b8842b
Pavel Litvinovich <21asdf@mail.ru>
2020-01-19 23:05:20 +0300

    Troelsen_encapsulation.1

---
+++ Troelsen_encapsulation_01/Troelsen_encapsulation_01/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Troelsen_encapsulation_01
+{
+    class Employee
+    {
+        private string empName;
+        private int empID;
+        private float cuppPay;
+        public Employee()
+        {
+        }
+        public Employee(string name, int id, float pay)
+        {
+            empName = name;
+            empID = id;
+            cuppPay = pay;
+        }
+        public void GiveBonus(float amount)
+        {
+            cuppPay += amount;
+        }
+        public void DisplayStatus()
+        {
+            Console.WriteLine($"NAME: {empName}");
+            Console.WriteLine($"ID: {empID}");
+            Console.WriteLine($"PAY: {cuppPay}");
+        }
+    }
+}
