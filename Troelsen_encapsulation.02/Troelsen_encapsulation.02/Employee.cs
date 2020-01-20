474d48afcf63e7494148de748d5111e8c4742a35
Pavel Litvinovich <21asdf@mail.ru>
2020-01-20 16:14:14 +0300

    Troelsen_encapsulation_02

---
+++ Troelsen_encapsulation.02/Troelsen_encapsulation.02/Employee.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Troelsen_encapsulation_02
+{
+    class Employee
+    {
+        private string empName;
+        private int empID;
+        private int empAge;
+        private float cuppPay;
+        public Employee()
+        {
+        }
+        public Employee(string name, int id, float pay) : this (name, 0, id, pay) { }
+        public Employee(string name, int id, int age, float pay)
+        {
+            Name = name;
+            ID = id;
+            Age = age;
+            Pay = pay;
+        }
+        public string Name
+        {
+            get => empName;
+            set => empName = value;
+        }
+        public int ID
+        {
+            get => empID;
+            set => empID = value;
+        }
+        public int Age
+        {
+            get => empAge;
+            set => empAge = value;
+        }
+        public float Pay
+        {
+            get => cuppPay;
+            set => cuppPay = value;
+        }
+        public void GiveBonus(float amount)
+        {
+            Pay += amount;
+        }
+        public void DisplayStatus()
+        {
+            Console.WriteLine($"NAME: {empName}");
+            Console.WriteLine($"ID: {empID}");
+            Console.WriteLine($"AGE: {empAge}");
+            Console.WriteLine($"PAY: {cuppPay}");
+        }
+    }
+}
---
+++ Troelsen_encapsulation.02/Troelsen_encapsulation.02/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Troelsen_encapsulation_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee("TOOOM", 2414, 30, 30000);
+            emp.DisplayStatus();
+
+            emp.Name = "AAA4XI";
+            emp.ID = 9379992;
+            emp.Age = 31;
+            emp.Pay = 33333;
+            emp.DisplayStatus();
+        }
+    }
+}
