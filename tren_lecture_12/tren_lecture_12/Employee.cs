e6035ae8873404ac5100c638d9cce1acfe74eb50
Pavel Litvinovich <21asdf@mail.ru>
2020-01-21 21:22:55 +0300

    lecture_12.(get-set)

---
+++ tren_lecture_12/tren_lecture_12/Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tren_lecture_12
+{
+    class Employee
+    {
+        private int age;
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                age = value;
+            }
+        }
+        public void MethodAge()
+        {
+            Employee employee = new Employee();
+            employee.Age = 11;
+
+            Console.WriteLine("1.  " + employee.Age);
+            Console.WriteLine("1.1 " + employee.age);
+        }
+        public void Info()
+        {
+            Employee employee = new Employee();
+            Console.WriteLine("2.  " + employee.Age);
+        }
+    }
+}
---
+++ tren_lecture_12/tren_lecture_12/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace tren_lecture_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.MethodAge();
+            employee.Info();
+            Console.WriteLine("3.  " + employee.Age);
+        }
+    }
+}
