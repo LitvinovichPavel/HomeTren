65e6b9659f408f0fdc644bd680237c79ca8500e7
Pavel Litvinovich <21asdf@mail.ru>
2020-01-27 19:07:03 +0300

    enheritance_metanit

---
+++ enheritance_metanit_1/enheritance_metanit_1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace enheritance_metanit_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Iiigor");
+            person.Display();
+            Employee employee = new Employee("Aaaliosha", "Google");
+            employee.Display();
+        }
+    }
+    class Person
+    {
+        public string Name { get; set; }
+        public Person(string name)
+        {
+            Name = name;
+        }
+        public void Display()
+        {
+            Console.WriteLine(Name);
+        }
+    }
+    class Employee : Person
+    {
+        public string Company { get; set;}
+        public Employee(string name, string company) : base(name)
+        {
+            Company = company;
+        }
+        public void Display()
+        {
+            Console.WriteLine(Name, Company);
+        }
+    }
+}
