14cf5598790ac1b82f75649739c63c55eb0668ab
Pavel Litvinovich <21asdf@mail.ru>
2020-01-27 16:05:48 +0300

    hm_11.1

---
+++ hm_11TREN/hm_11TREN/Figures.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace hm_11TREN
+{
+    abstract class Figures
+    {
+        public string Name { get; set; }
+        public Figures(string name)
+        {
+            Name = name;
+        }
+        public abstract double figiureArea();
+    }
+
+    class Square : Figures
+    {
+        private double Storona { get; set; }
+        public Square(string name, double storona) : base(name)
+        {
+            Storona = storona;
+        }
+
+        public override double figiureArea()
+        {
+            return Math.Pow(Storona, 2);
+        }
+    }
+    class Triangle : Figures
+    {
+        private double Sides { get; set; }
+        private double TriangleBase { get; set; }
+
+        public Triangle(string name, double sides, double triangleBase) : base(name)
+        {
+            Sides = sides;
+            TriangleBase = triangleBase;
+        }
+
+        public override double figiureArea()
+        {
+            double high = 0.5 * Math.Sqrt(4 * Math.Pow(Sides, 2) - Math.Pow(TriangleBase, 2));
+            return Math.Round(0.5 * TriangleBase * high);
+        }
+    }
+    class Circle : Figures
+    {
+        private double Radius { get; set; }
+
+        public Circle(string name, double radius) : base(name)
+        {
+            Radius = radius;
+        }
+
+        public override double figiureArea()
+        {
+            return Math.Round(Math.Pow(Radius, 2) * Math.PI);
+        }
+    }
+}
---
+++ hm_11TREN/hm_11TREN/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace hm_11TREN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            Figures[] mas = new Figures[3];
+
+            mas[0] = new Square("Square", rand.Next(1, 10));
+            mas[1] = new Triangle("Triangle", rand.Next(1, 10), rand.Next(1, 10));
+            mas[2] = new Circle("Circle", rand.Next(1, 10));
+
+            Result(mas);
+        }
+        public static void Result(Figures[] mas)
+        {
+            foreach (var item in mas)
+            {
+                Console.WriteLine($"This is {item.Name}.CLR Type is {item.GetType()}.Square is {item.figiureArea()}");
+            }
+        }
+    }
+}
