218162ebf2e64c9ca65773dcaeedb7d1e64d442f
Pavel Litvinovich <21asdf@mail.ru>
2020-01-30 17:31:11 +0300

    property

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Info info = new Info();
+            info.InfoMethod();
+        }
+    }
+    class Info
+    {
+        public void InfoMethod()
+        {
+            GetSet getSet = new GetSet();
+            getSet.Method(1200, 500);
+            getSet.Name = "AAA";
+        }
+    }
+    class GetSet
+    {
+        public int Salary { get; set; }
+        public int Costs { get; set; }
+        private string _name;
+        public int Balance { get; set; }
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public int Method(int Salary, int Costs)
+        {
+            return Balance = Salary - Costs;
+        }
+    }
+}
