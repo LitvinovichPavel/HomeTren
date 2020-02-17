459abbeee127cbc6bb1a8aed2ceb2cd295752210
Pavel Litvinovich <21asdf@mail.ru>
2020-02-17 21:40:32 +0300

    List.year.salary.1

---
+++ _List/_List/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> man = new List<Employee>() { };
+            man.Add(new Employee(3000, 2));
+            man.Add(new Employee(5000, 6));
+
+            foreach (var item in man)
+            {
+                Console.WriteLine($"Year: {item.Year}, salary {item.Salary}");
+            }
+        }
+    }
+    class Employee
+    {
+        public int Salary { get; set; }
+        public int Year { get; set; }
+        public Employee(int salary, int year)
+        {
+            Salary = salary;
+            Year = year;
+        }
+    }
+}
