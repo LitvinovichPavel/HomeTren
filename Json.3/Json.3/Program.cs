1ff8205f58c8a23355244ce1f539ac1d4a813dec
Pavel Litvinovich <21asdf@mail.ru>
2020-02-20 15:38:18 +0300

    Json.20.02.2020.1

--- Json.3/Json.3/Program.cs
+++ Json.3/Json.3/Program.cs
@@ -9,52 +9,58 @@ namespace Json._3
     {
         static async Task Main(string[] args)
         {
+            /*
             Menu menu = new Menu();
             menu.Makis = new Maki[4];
 
             menu.Makis[0] = new Maki()
             {
                 Title = "Avokado maki",
-                Mass = 100,
+                Mass = 110,
                 Price = 5.2
             };
             menu.Makis[1] = new Maki()
             {
                 Title = "Arizona maki",
-                Mass = 100,
+                Mass = 120,
                 Price = 4.8
             };
             menu.Makis[2] = new Maki()
             {
                 Title = "Karada maki",
-                Mass = 100,
+                Mass = 130,
                 Price = 3.8
             };
             menu.Makis[3] = new Maki()
             {
                 Title = "Nara maki",
-                Mass = 100,
+                Mass = 140,
                 Price = 7.2
-            };
+            };*/
 
             var options = new JsonSerializerOptions
             {
                 WriteIndented = true
             };
+            /*
             // сохранение данных
             using (FileStream fs = new FileStream("menuMaki.json", FileMode.OpenOrCreate))
             {
                 await JsonSerializer.SerializeAsync<Menu>(fs, menu, options);
-            }
+            }*/
 
             // чтение данных
             using (FileStream fs = new FileStream("menuMaki.json", FileMode.OpenOrCreate))
             {
                 Menu restoredMenu = await JsonSerializer.DeserializeAsync<Menu>(fs, options);
-                Console.WriteLine($"MENU: {restoredMenu.Makis[0].Title}");
+                Console.WriteLine($"MENU: {restoredMenu.Makis[0].Title}, {restoredMenu.Makis[0].Mass}gr, PRICE: {restoredMenu.Makis[0].Price}");
+                Console.WriteLine($"MENU: {restoredMenu.Makis[1].Title}, {restoredMenu.Makis[1].Mass}gr, PRICE: {restoredMenu.Makis[1].Price}");
+                Console.WriteLine($"MENU: {restoredMenu.Makis[2].Title}, {restoredMenu.Makis[2].Mass}gr, PRICE: {restoredMenu.Makis[2].Price}");
+                Console.WriteLine($"MENU: {restoredMenu.Makis[3].Title}, {restoredMenu.Makis[3].Mass}gr, PRICE: {restoredMenu.Makis[3].Price}");
             }
         }
     }
+
     class Menu
     {
         public Maki[] Makis { get; set; }
