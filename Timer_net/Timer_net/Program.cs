9ae45f1cd9419d4913993abee2ae9e28e357100e
Pavel Litvinovich <21asdf@mail.ru>
2020-03-12 20:03:11 +0300

    Troelsen_auto_property.1

---
+++ Timer_net/Timer_net/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+
+namespace Timer_net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            // устанавливаем метод обратного вызова
+            TimerCallback tm = new TimerCallback(Count);
+            // создаем таймер
+            Timer timer = new Timer(tm, num, 0, 2000);
+
+            Console.ReadLine();
+        }
+        public static void Count(object obj)
+        {
+            Console.WriteLine(DateTime.Now);
+        }
+    }
+}
---
+++ Troelsen_autom_property/Troelsen_autom_property/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Troelsen_autom_property
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car c = new Car();
+            c.PetName = "Frank";
+            c.Speed = 55;
+            c.Color = "Red";
+            c.DisplayStatus();
+
+            Garage g = new Garage();
+            g.MyAuto = c;
+            Console.WriteLine($"{g.NumerOfCars}, {g.MyAuto.PetName}");
+        }
+    }
+    class Car
+    {
+        public string PetName { get; set; }
+        public int Speed { get; set; }
+        public string Color { get; set; }
+        public void DisplayStatus()
+        {
+            Console.WriteLine($"{PetName}, {Speed}, {Color}");
+        }
+    }
+    class Garage
+    {
+        public int NumerOfCars { get; set; }
+        public Car MyAuto { get; set;  }
+        public Garage()
+        {
+            MyAuto = new Car();
+            NumerOfCars = 1;
+        }
+        public Garage(Car car, int number)
+        {
+            MyAuto = car;
+            NumerOfCars = number;
+        }
+    }
+}
