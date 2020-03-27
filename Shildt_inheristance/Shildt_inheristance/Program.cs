ce0943833aab908c1a037ea0b5e7179e05c41755
21asdfforitacademy <21asdfforitacademy@mail.ru>
2020-03-27 19:06:47 +0300

    Shildt_inheritance

---
+++ Shildt_inheristance/Shildt_inheristance/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Shildt_inheristance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
